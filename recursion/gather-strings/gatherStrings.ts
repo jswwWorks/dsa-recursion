6117464f62d1ef09a02296676235d3ef5272a1f9
Teagan McClintock <teagan.mcclintock@gmail.com>
2024-02-27 04:32:56 -0500

    Added solution for gatherStrings that passes tests but may still fail in edge cases.

--- recursion/gather-strings/gatherStrings.ts
+++ recursion/gather-strings/gatherStrings.ts
@@ -1,7 +1,48 @@
+"use strict";
+
 /** gatherStrings: given an object, return array of all the string values. */
 
 function gatherStrings(obj: Record<string, any>): string[] {
-  return ["x"];
+
+  const valueArray: string[] = [];
+
+  for (const key in obj) {
+    if ((typeof obj[key]) === 'string') valueArray.push(obj[key]);
+    else if (
+        (
+          typeof obj[key] === 'object'
+          && !Array.isArray(obj[key])
+        )
+      ){
+      valueArray.push(...gatherStrings(obj[key]));
+    }
+
+  }
+
+  return valueArray;
+
+  /**
+   * Base case: no unaccessed keys in object
+   *
+   * nestedObj = {
+    firstName: "Lester",
+    favoriteNumber: 22,
+    moreData: {
+      lastName: "Testowitz",
+    },
+    funFacts: {
+      moreStuff: {
+        anotherNumber: 100,
+        deeplyNestedString: {
+          almostThere: {
+            success: "you made it!",
+          },
+        },
+      },
+      favoriteString: "nice!",
+    },
+  }
+   */
 }
 
 export { gatherStrings };
