58fcacc04b05f3bbe9f298fffbbe3567c28bd373
jswwWorks <jsww.works@gmail.com>
2024-02-27 18:05:11 -0500

    Solved reverse string and feeble attempt at find index (ok, valiant effort with something to show for it. we have a base case, maybe?)

--- recursion/find-index/findIndex.ts
+++ recursion/find-index/findIndex.ts
@@ -1,6 +1,15 @@
 /** findIndex: return index of val in arr (or -1 if val is not present). */
 
 function findIndex(arr: number[], val: number): number {
+
+  if (arr.length === 0) return -1;
+
+  if (arr[0] === val) return Index
+
+  return findIndex(arr.slice(1), val);
+
+
+
   return 42;
 }
 
--- recursion/rev-string/revString.ts
+++ recursion/rev-string/revString.ts
@@ -1,7 +1,37 @@
 /** revString: return a copy of a string, but in reverse. */
 
 function revString(str: string): string {
-  return "x";
+
+  /**
+   *  Little example for later:
+   *
+   *  "phineas"
+   *
+   *  - not empty
+   *
+   *  everythingButLastLetter gets called, sending in "phinea"
+   *    - (keeps on getting called with "phine", "phin", "phi", "ph", "p", "")
+   *
+   *  returns "" when "" is sent through as a param
+   *
+   *    - goes back to "p" instance, and returns "p" + ""
+   *    - goes back to "ph", and returns "h" + "p"
+   *    ...
+   *
+   *  until it eventually returns "saenihp"
+   *
+   */
+
+  if (str === "") return "";
+
+  // Shorter - stepping through forwards
+  return revString(str.slice(1)) + str[0];
+
+  // Approach in example - stepping through backwards
+  const everythingButTheLastLetter: string = revString(str.slice(0, -1));
+
+  return str[str.length - 1] + everythingButTheLastLetter;
+
 }
 
 export { revString };
