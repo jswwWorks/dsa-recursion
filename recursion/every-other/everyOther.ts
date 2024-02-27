65e149f8a3c2ed2554a8b4437899a8c13ef0e9b5
jswwWorks <jsww.works@gmail.com>
2024-02-27 15:10:34 -0500

    Wrote product function and added pseudocode to everyOther

--- recursion/every-other/everyOther.ts
+++ recursion/every-other/everyOther.ts
@@ -2,6 +2,15 @@
 
 function everyOther(str: string): string {
   return "x";
+
+  // base case: string is empty or hit end of string
+
+  // ideally, not mutating original string
+
+  // odd idx-ed chars we include, otherwise not
+  // use concatenation when calling recursively
+
+
 }
 
 export { everyOther };
--- recursion/product/product.ts
+++ recursion/product/product.ts
@@ -3,7 +3,11 @@ import { LLNodeNum } from "../common/ll";
 /** product: calculate the product of a linked list of numbers. */
 
 function product(nums: LLNodeNum | null): number {
-  return 42;
+
+  // nums is either a node or null despite being plural
+  if (nums === null) return 1; // has to be 1 because it's multiplication
+
+  return nums.val * product(nums.next);
 }
 
 export { product };
