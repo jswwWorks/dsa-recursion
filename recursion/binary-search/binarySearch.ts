948a84aaaff16cceebc63ea06a81384a2e5bdf2c
jswwWorks <jsww.works@gmail.com>
2024-02-27 19:06:32 -0500

    Solved binarySearch problem.

--- recursion/binary-search/binarySearch.ts
+++ recursion/binary-search/binarySearch.ts
@@ -2,7 +2,19 @@
  * return true if val is in array, false if not present). */
 
 function binarySearch(arr: number[], val: number, left = 0, right = arr.length): boolean {
-  return false;
+
+  if ((left === right) || (left === arr.length - 1)) return false;
+
+  const idx: number = Math.floor((left + right) / 2);
+
+  if (arr[idx] === val) return true;
+
+  else if (arr[idx] > val) {
+    return binarySearch(arr, val, left, right = idx);
+  } else {
+    return binarySearch(arr, val, left = idx, right);
+  }
+
 }
 
 export { binarySearch };
