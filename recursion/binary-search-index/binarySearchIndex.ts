2f978dbd9308c586efb14c78020c2c273e084b05
jswwWorks <jsww.works@gmail.com>
2024-02-27 19:08:46 -0500

    Solved binary Search Index

--- recursion/binary-search-index/binarySearchIndex.ts
+++ recursion/binary-search-index/binarySearchIndex.ts
@@ -2,11 +2,25 @@
  * return the index of that value (or -1 if val is not present). */
 
 function binarySearchIndex(
-    arr: number[],
-    val: number,
-    left = 0,
-    right = arr.length): number {
-  return 42;
+  arr: number[],
+  val: number,
+  left = 0,
+  right = arr.length): number {
+
+  if ((left === right) || (left === arr.length - 1)) return -1;
+
+  const idx: number = Math.floor((left + right) / 2);
+
+  if (arr[idx] === val) return idx;
+
+  else if (arr[idx] > val) {
+    return binarySearchIndex(arr, val, left, right = idx);
+  } else {
+    return binarySearchIndex(arr, val, left = idx, right);
+  }
+
+
+
 }
 
 export { binarySearchIndex };
